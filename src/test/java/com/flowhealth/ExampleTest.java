0450062ebcf819e6b5dc663e3f6098f860485c14
AlmasN <95028680+Exiliahs@users.noreply.github.com>
2022-09-13 00:06:49 +0600

    First commit from console

---
+++ src/test/java/com/flowhealth/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.flowhealth;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    void exTest() {
+        Assertions.assertTrue(3>5);
+    }
+}
